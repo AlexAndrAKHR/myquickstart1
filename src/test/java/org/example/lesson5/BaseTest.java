82511569f382a635253092b9852901785bc4eef6
AlexAndrAKHR <aalexaandraa76@gmail.com>
2022-02-04 18:06:11 +0300

    lesson6

--- src/test/java/org/example/lesson5/BaseTest.java
+++ src/test/java/org/example/lesson5/BaseTest.java
@@ -3,13 +3,17 @@ package org.example.lesson5;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
-    WebDriver webDriver;
+    protected static final String URL = "https://www.korablik.ru/";
+    protected WebDriver webDriver;
+
+
 
     @BeforeEach
     void setUp() {
