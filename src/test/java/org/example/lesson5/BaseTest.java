07482fedb8997653b4e370e92070f0e3215d0b6a
AlexAndrAKHR <aalexaandraa76@gmail.com>
2022-02-08 18:48:58 +0300

    lesson7

--- src/test/java/org/example/lesson5/BaseTest.java
+++ src/test/java/org/example/lesson5/BaseTest.java
@@ -1,17 +1,24 @@
 package org.example.lesson5;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Allure;
+import org.example.lesson6.listener.ActionEventListener;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import java.util.concurrent.TimeUnit;
 
+import static io.qameta.allure.Allure.step;
+
 public class BaseTest {
     protected static final String URL = "https://www.korablik.ru/";
-    protected WebDriver webDriver;
+    protected EventFiringWebDriver webDriver;
 
 
 
@@ -20,13 +27,21 @@ public class BaseTest {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--blink-settings=imagesEnabled=false");
 
-        webDriver = WebDriverManager.chromedriver().capabilities(chromeOptions).create();
+        webDriver = new EventFiringWebDriver (WebDriverManager.chromedriver().capabilities(chromeOptions).create());
+        webDriver.register(new ActionEventListener());
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
     }
 
     @AfterEach
     void tearDown() {
+        step("Отображение логов браузера", ()  -> {
+            for (LogEntry logEntry : webDriver.manage().logs().get(LogType.BROWSER)) {
+                Allure.addAttachment("Logs", logEntry.getMessage());
+
+            }
+        });
+
         webDriver.quit();
     }
 }
